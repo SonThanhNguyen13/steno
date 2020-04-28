58ec07e772fb16c0554f4a22fd75eb95b2c7ce63
sonthanhnguyen13 <sonsieunhan2098@gmail.com>
2020-04-28 21:29:57 +0700

    Fix

--- decr.py
+++ decr.py
@@ -13,12 +13,14 @@ parser.add_argument('-m', '--matrix', required = 'True', help='Difference Matrix
 parser.add_argument('-l', '--length', required = 'True', help='Message Length')
 args = parser.parse_args()
 
+
 def read_key_and_iv(key_file, iv_file):
     with open(key_file, 'rb') as f:
         key = f.readline()
     with open(iv_file, 'rb') as f:
         iv = f.readline()
     return key, iv
+
 
 def decrypt_message(ciphertext, key, iv):
     message = []
@@ -31,8 +33,36 @@ def decrypt_message(ciphertext, key, iv):
     message = [int(i, 2) for i in message]
     message = bytearray(message)
     message = bytes(message)
-    obj = AES.new(key, AES.MODE_CBC, iv=iv)
-    return(obj.decrypt(message))
+    decr = AES.new(key, AES.MODE_CBC, iv=iv)
+    return(decr.decrypt(message))
+
+
+def to_list(matrix):
+    for i in range(len(matrix)):
+        matrix[i] = matrix[i].tolist()
+    return matrix
+
+
+def to_bin(matrix):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            for k in range(len(matrix[i][j])):
+                matrix[i][j][k] = round(matrix[i][j][k])
+                matrix[i][j][k] = bin(matrix[i][j][k]).replace('0b', "").zfill(8)
+    return matrix
+
+
+def get_ciphertext(matrix, length):
+    rows = len(matrix[1])
+    num = len(matrix[1][1])
+    ciphertext = ''
+    for j in range(rows):
+        for k in range(num):
+            for i in range(1,4):
+                ciphertext += matrix[i][j][k][-1]
+                if len(ciphertext) == length * 8:
+                    return ciphertext
+
 
 def decrypt(image, matrix, length):
     img = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
@@ -45,28 +75,17 @@ def decrypt(image, matrix, length):
     result.append(LH)
     result.append(HL)
     result.append(HH)
-    for i in range(len(result)):
-        result[i] = result[i].tolist()
-    for i in range(len(result)):
-        for j in range(len(result[i])):
-            for k in range(len(result[i][j])):
-                result[i][j][k] = round(result[i][j][k])
-                result[i][j][k] = bin(result[i][j][k]).replace('0b', "").zfill(8)
-    rows = len(result[1])
-    num = len(result[1][1])
-    index = 0
-    ciphertext = ''
-    for j in range(rows):
-        for k in range(num):
-            for i in range(1,4):
-                ciphertext += result[i][j][k][-1]
-                if len(ciphertext) == length * 8:
-                    return ciphertext
+    result = to_list(result)
+    result = to_bin(result)
+    ciphertext = get_ciphertext(result, length=length)
+    return ciphertext
+
 
 def read_msg_length(file):
     with open(file, 'r') as f:
         msg_len = f.readline()
         return int(msg_len)
+
 
 def main():
     start = time.time()
@@ -74,9 +93,10 @@ def main():
     length = read_msg_length(args.length)
     ciphertext = decrypt(args.image, args.matrix, length)
     message = decrypt_message(ciphertext, key, iv)
-    print(message)
+    print('Message: {}'.format(str(message).replace("b'", "")))
     end = time.time()
     print("Decrypt message in: {:.2f} s".format(end - start))
+
 
 if __name__ == '__main__':
     main()
--- enc.py
+++ enc.py
@@ -5,7 +5,6 @@ from Crypto.Cipher import AES
 import cv2
 import numpy as np
 import pywt
-from PIL import Image
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--image', required='True', help='Image')
@@ -14,6 +13,7 @@ parser.add_argument('-v', '--iv', required='True', help='AES initilization vecto
 parser.add_argument('-s', '--save', default='result', help = 'Result dir')
 args = parser.parse_args()
 
+
 def read_key_and_iv(key_file, iv_file):
     with open(key_file, 'r') as f:
         key = f.readline()
@@ -21,61 +21,97 @@ def read_key_and_iv(key_file, iv_file):
         iv = f.readline()
     return key, iv
 
+
 def encrypt_message(message, key, iv):
     key = bytes(key, 'utf-8')
     iv = bytes(iv, 'utf-8')
     message = bytes(message, 'utf-8')
-    obj = AES.new(key, AES.MODE_CBC, iv=iv)
-    ciphertext = obj.encrypt(message)
+    enc = AES.new(key, AES.MODE_CBC, iv=iv)
+    ciphertext = enc.encrypt(message)
     ciphertext = "".join([bin(i)[2:].zfill(8) for i in ciphertext])
     return ciphertext
+
+
+def to_int(matrix):
+    for i in range(len(matrix)):
+        matrix[i] = matrix[i].astype('int8')
+    return matrix
+
+
+def to_list(matrix):
+    for i in range(len(matrix)):
+        matrix[i] = matrix[i].tolist()
+    return matrix
+
+
+def to_bin(matrix):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            for k in range(len(matrix[i][j])):
+                matrix[i][j][k] = bin(matrix[i][j][k]).replace('0b', "").zfill(8)
+    return matrix
+
+
+def bin_to_int(matrix):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            for k in range(len(matrix[i][j])):
+                matrix[i][j][k] = int(matrix[i][j][k], 2)
+    return matrix
+
+
+def stegano(matrix, ciphertext):
+    j = 0
+    k = 0
+    row_len = len(matrix[1][1])
+    for index, item in enumerate(ciphertext):
+        if index % 3 == 0:
+            matrix[1][j][k] = matrix[1][j][k][:-1] + item
+        elif index % 3 == 1:
+            matrix[2][j][k] = matrix[2][j][k][:-1] + item
+        else:
+            matrix[3][j][k] = matrix[3][j][k][:-1] + item
+            k += 1
+            if k == row_len:
+                j += 1
+                k = 0
+    return matrix
+
+
+def to_np_array(matrix):
+    for i in range(len(matrix)):
+        matrix[i] = np.array(matrix[i])
+    return matrix
+
 
 def encrypt(img, ciphertext):
     coeffs2 = pywt.dwt2(img, 'haar')
     LL, (LH, HL, HH) = coeffs2
-    result = []
-    result.append(LL)
-    result.append(LH)
-    result.append(HL)
-    result.append(HH)
-
-    for i in range(len(result)):
-        result[i] = result[i].astype('int64')
-        result[i] = result[i].tolist()
-    for i in range(len(result)):
-        for j in range(len(result[i])):
-            for k in range(len(result[i][j])):
-                result[i][j][k] = bin(result[i][j][k]).replace('0b', "").zfill(8)
-    rows = len(result[1])
-    num = len(result[1][1])
-    index = 0
-    try: 
-        for j in range(rows):
-            for k in range(num):
-                for i in range(1,4):
-                    result[i][j][k] = result[i][j][k][:-1] + ciphertext[index]
-                    index += 1
-    except IndexError:
-        pass
-    for i in range(len(result)):
-        for j in range(len(result[i])):
-            for k in range(len(result[i][j])):
-                result[i][j][k] = int(result[i][j][k], 2)
-
-    for i in range(len(result)):
-        result[i] = np.array(result[i])
-    data = (result[0], (result[1], result[2], result[3]))
+    sub_imgs = []
+    sub_imgs.append(LL)
+    sub_imgs.append(LH)
+    sub_imgs.append(HL)
+    sub_imgs.append(HH)
+    sub_imgs = to_int(sub_imgs)
+    sub_imgs = to_list(sub_imgs)
+    sub_imgs = to_bin(sub_imgs)
+    sub_imgs = stegano(sub_imgs, ciphertext)
+    sub_imgs = bin_to_int(sub_imgs)
+    sub_imgs = to_np_array(sub_imgs)
+    data = (sub_imgs[0], (sub_imgs[1], sub_imgs[2], sub_imgs[3]))
     rmk_origin = pywt.idwt2(data, 'haar')
     rmk = rmk_origin.astype('uint8')
     H = rmk_origin - rmk
     return rmk, H
     
+
 def padding(message):
     if len(message) % 16 == 0:
         return message
     else:
         n = math.ceil(len(message) / 16)
         return message.ljust(n * 16)
+
 
 def main():
     message = input('Insert message: ')
@@ -94,8 +130,6 @@ def main():
     print("Message's length: {}/len.txt".format(args.save))
     end = time.time()
     print("Encrpyt message in: {:.2f} s".format(end - start))
-    img = Image.fromarray(steno_img)
-    img.show()
 
 
 if __name__ == '__main__':
